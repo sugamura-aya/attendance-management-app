54ae495c188cfcf9a958c7ce11bb23e2f1582416
sugamura aya <it.is.a.brand.new.world825@gmail.com>
2026-02-18 16:28:33 +0900

    AttendanceRequestController記述

--- src/app/Http/Controllers/AttendanceRequestController.php
+++ src/app/Http/Controllers/AttendanceRequestController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;// フォームの入力値などをまとめて運ぶためのコード
 use Illuminate\Support\Facades\Auth; 
-use Carbon\Carbon; // 日付や時刻の計算・加工に必須
-use App\Models\Attendance; // どの勤怠を直したいか特定するため必須
 use App\Models\AttendanceRequest; 
-use App\Models\BreakTimeRequest; 
 
 class AttendanceRequestController extends Controller
 {
-    //
+    public function index()
+    {
+        // 1. 承認待ちに絞る（自分の分 ＆ 未承認）
+        $pendingRequests = AttendanceRequest::where('user_id', Auth::id())
+            ->with('user') // 申請者の情報
+            ->where('status', 0)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        // 2. 承認済みに絞る（自分の分 ＆ 承認済）
+        $approvedRequests = AttendanceRequest::where('user_id', Auth::id())
+            ->with('user') // 申請者の情報
+            ->where('status', 1)
+            ->orderBy('created_at', 'desc')
+            ->get();
+
+        // 3. 1,2両方をViewに返す
+        return view('attendance_request.list', compact('pendingRequests', 'approvedRequests'));
+    }
 }
