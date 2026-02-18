4d74445a84a254a031ce21c8d15dbf4329424902
sugamura aya <it.is.a.brand.new.world825@gmail.com>
2026-02-18 17:03:16 +0900

    AdminAuthController作成

--- src/app/Http/Controllers/AdminAuthController.php
+++ src/app/Http/Controllers/AdminAuthController.php
@@ -4,10 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 use App\Http\Requests\AdminLoginRequest;
 
 class AdminAuthController extends Controller
 {
-    //
+    // ➆ 管理者ログイン画面（表示）
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
+
+
+    // ログイン処理（POST）
+    public function login(AdminLoginRequest $request)
+    {
+        // 1. メアドとパスワードを受け取る
+        $credentials = $request->only('email', 'password');
+
+        // 2. 相違なければ管理者ページへ
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('admin.attendance.list');
+        }
+
+        // 3. ダメだったらエラーを抱えて戻る
+        return back()->withInput()->withErrors([
+            'email' => 'ログイン情報が正しくありません。',
+        ]);
+    }
+
+
+    // ログアウト処理（POST）
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        return redirect()->route('admin.login');
+    }
 }
